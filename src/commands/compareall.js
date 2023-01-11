8f48b4ef0646b2005faae9da4cc64db4fbfe6e07
cosmicThreePointO <cosmicvoid@mechverse.co>
2023-01-11 15:16:35 -0800

    to fixed 2

--- src/commands/compareall.js
+++ src/commands/compareall.js
@@ -4,7 +4,7 @@ import data from "../../data/data.json" assert { type: "json" };
 const CompareAllCommand = new SlashCommandBuilder()
   .setName("compareall")
   .setDescription(
-    "Gives the difference between PSA10 and PSA9 for each card in the set"
+    "Gives the difference in % between PSA10 and PSA9 for each card in the set"
   );
 
 export default CompareAllCommand.toJSON();
