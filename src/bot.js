fa93e438908f4397e68a61c5a178fed836bf739b
cosmicThreePointO <cosmicvoid@mechverse.co>
2023-01-11 16:18:14 -0800

    invert the sorting

--- src/bot.js
+++ src/bot.js
@@ -57,7 +57,7 @@ const differencePokemons = Object.entries(data)
     return [key["1"]["name"], difference.toFixed(2)];
   })
   .sort(function (a, b) {
-    return a[1] - b[1];
+    return b[1] - a[1];
   });
 
 console.log("difference: ", differencePokemons);
