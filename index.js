41686414c475d69c2a0d98e5a48808abf813f9a0
cosmicThreePointO <cosmicvoid@mechverse.co>
2023-02-01 16:38:50 -0800

    take out config

--- index.js
+++ index.js
@@ -32,10 +32,7 @@ async function run() {
 
       spinner.text = "Launching headless browser page";
 
-      const browser = await puppeteer.launch({
-        headless: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      });
+      const browser = await puppeteer.launch();
       const page = await browser.newPage();
       await page.setDefaultNavigationTimeout(TIMEOUT_LIMIT);
       await page.setDefaultTimeout(TIMEOUT_LIMIT);
