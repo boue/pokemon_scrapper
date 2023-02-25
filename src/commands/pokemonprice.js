2d38670413c46cf4cfb33381d0761a23e3c2fba9
cosmicThreePointO <cosmicvoid@mechverse.co>
2023-02-24 21:52:51 -0800

    test

--- src/commands/pokemonprice.js
+++ src/commands/pokemonprice.js
@@ -37,14 +37,14 @@ export const PokemonPriceCommand = new SlashCommandBuilder()
         .setAutocomplete(true)
     // .setRequired(true)
   )
-  .addStringOption(
-    (option) =>
-      option
-        .setName("jungle")
-        .setDescription("Search a card in Jungle 1st Edition")
-        .setAutocomplete(true)
-    // .setRequired(true)
-  )
+  // .addStringOption(
+  //   (option) =>
+  //     option
+  //       .setName("jungle")
+  //       .setDescription("Search a card in Jungle 1st Edition")
+  //       .setAutocomplete(true)
+  //   // .setRequired(true)
+  // )
   .addStringOption(
     (option) =>
       option
@@ -61,57 +61,57 @@ export const PokemonPriceCommand = new SlashCommandBuilder()
 
 export const autocomplete = async (interaction) => {
   console.log("inside autocomplete");
-  const focusedOption = interaction.options.getFocused(true);
-  let choices;
+  const focusedOption = interaction.options.getFocused();
+  const choices = [
+    "Charizard Holo 4",
+    "Blastoise Holo 2",
+    "Venusaur Holo 15",
+    "Nidoking Holo 11",
+    "Chansey Holo 3",
+    "Clefairy Holo 5",
+    "Alakazam Holo 1",
+    "Poliwrath Holo 13",
+    "Raichu Holo 14",
+    "Ninetales Holo 12",
+    "Hitmonchan Holo 7",
+    "Mewtwo Holo 10",
+    "Zapdos Holo 16",
+    "Gyarados Holo 6",
+    "Magneton Holo 9",
+  ];
 
-  if (focusedOption.name === "base") {
-    choices = [
-      "Charizard Holo 4",
-      "Blastoise Holo 2",
-      "Venusaur Holo 15",
-      "Nidoking Holo 11",
-      "Chansey Holo 3",
-      "Clefairy Holo 5",
-      "Alakazam Holo 1",
-      "Poliwrath Holo 13",
-      "Raichu Holo 14",
-      "Ninetales Holo 12",
-      "Hitmonchan Holo 7",
-      "Mewtwo Holo 10",
-      "Zapdos Holo 16",
-      "Gyarados Holo 6",
-      "Magneton Holo 9",
-    ];
-  }
-
-  if (focusedOption.name === "jungle") {
-    choices = [
-      "Electrode Holo 2",
-      "Flareon Holo 3",
-      "Jolteon Holo 4",
-      "Kangaskhan Holo 5",
-      "Mr. Mime Holo 6",
-      "Nidoqueen Holo 7",
-      "Pidgeot Holo 8",
-      "Pinsir Holo 9",
-      "Scyther Holo 10",
-      "Snorlax Holo 11",
-      "Vaporeon Holo 12",
-      "Venomoth Holo 13",
-      "Victreebel Holo 14",
-      "Vileplume Holo 15",
-      "Wigglytuff Holo 16",
-    ];
-  }
-  console.log("these are the choices: ", choices);
-  const filtered = choices.filter((choice) =>
-    choice.startsWith(focusedOption.value)
-  );
+  const filtered = choices.filter((choice) => choice.startsWith(focusedValue));
+  // const filtered = choices.filter((choice) =>
+  //   choice.startsWith(focusedOption.value)
+  // );
   console.log("these are the filtered choices: ", filtered);
   await interaction.respond(
     filtered.map((choice) => ({ name: choice, value: choice }))
   );
 };
+
+// if (focusedOption.name === "jungle") {
+//   choices = [
+//     "Electrode Holo 2",
+//     "Flareon Holo 3",
+//     "Jolteon Holo 4",
+//     "Kangaskhan Holo 5",
+//     "Mr. Mime Holo 6",
+//     "Nidoqueen Holo 7",
+//     "Pidgeot Holo 8",
+//     "Pinsir Holo 9",
+//     "Scyther Holo 10",
+//     "Snorlax Holo 11",
+//     "Vaporeon Holo 12",
+//     "Venomoth Holo 13",
+//     "Victreebel Holo 14",
+//     "Vileplume Holo 15",
+//     "Wigglytuff Holo 16",
+//   ];
+// }
+// console.log("these are the choices: ", choices);
+
+// };
 // module.exports = {
 //   data: new SlashCommandBuilder()
 //     .setName("price")
