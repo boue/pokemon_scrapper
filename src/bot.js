bdbe236a6d1287e4d614ceccabe42facb1547a62
cosmicThreePointO <cosmicvoid@mechverse.co>
2023-02-26 15:16:54 -0800

    make price bot

--- src/bot.js
+++ src/bot.js
@@ -5,7 +5,11 @@ import { Routes } from "discord-api-types/v10";
 import data from "../data/dataList.json" assert { type: "json" };
 
 // import MarketPriceCommand from "./commands/marketprice.js";
-import { PokemonPriceCommand, autocomplete } from "./commands/pokemonprice.js";
+import {
+  PokemonPriceCommand,
+  autocomplete,
+  execute,
+} from "./commands/pokemonprice.js";
 // import PsaComparisonCommand from "./commands/psacomparison.js";
 // import MarketCapCommand from "./commands/marketcap.js";
 // import TotalMarketCap from "./commands/totalmarketcap.js";
@@ -21,11 +25,11 @@ const CLIENT_ID = process.env.DISCORDJS_CLIENT_ID;
 const GUILD_ID = process.env.DISCORDJS_GUILD_ID;
 const CHANNEL_ID = process.env.DISCORDJS_CHANNEL_ID;
 
-const findCard = (name, set) => {
-  const cardsInSet = data[set]?.cards;
-  const card = cardsInSet.find((card) => card.name === name);
-  return card;
-};
+// const findCard = (name, set) => {
+//   const cardsInSet = data[set]?.cards;
+//   const card = cardsInSet.find((card) => card.name === name);
+//   return card;
+// };
 
 const client = new Client({
   intents: [
@@ -43,268 +47,40 @@ client.on("ready", () => {
 });
 
 client.on("interactionCreate", async (interaction) => {
-  // if (!interaction.isCommand()) return;
-  // console.log("interaction is a command!!");
-  // if (!interaction.isAutocomplete()) return;
-  // console.log("interaction is autocomplete!!");
-
-  // const command = interaction.client.commands.get(interaction.commandName);
-
-  // if (!command) {
-  //   console.error(`No command matching ${interaction.commandName} was found.`);
-  //   return;
-  // }
   try {
     if (interaction.isAutocomplete()) {
       console.log("inside autocomplete");
       await autocomplete(interaction);
     }
-    // if (interaction.commandName === "marketprice") {
-    //   const set = interaction.options.getString("set");
-    //   const cardsInSet = data[set]?.cards;
-
-    //   const formattedPokemons = cardsInSet
-    //     .map(({ name, psa10, psa9, pop10, pop9, raw }) => {
-    //       return (
-    //         "\n" +
-    //         "Name: " +
-    //         name +
-    //         "\n" +
-    //         "Current PSA10 Price: " +
-    //         "$" +
-    //         psa10 +
-    //         "\n" +
-    //         "Current PSA9 Price: " +
-    //         "$" +
-    //         psa9 +
-    //         "\n" +
-    //         "Current POP10: " +
-    //         pop10 +
-    //         "\n" +
-    //         "Current POP9: " +
-    //         pop9 +
-    //         "\n" +
-    //         "\n"
-    //       );
-    //     })
-    //     .join("");
-
-    //   await interaction.reply(formattedPokemons);
-    // }
-    // if (interaction.commandName === "psaspread") {
-    //   const target1 = interaction.options.getString("pokemon1");
-    //   const pokemon1 = findCard(
-    //     target1.split(" - ")[0],
-    //     target1.split(" - ")[1]
-    //   );
-    //   const target2 = interaction.options.getString("pokemon2");
-    //   const pokemon2 = findCard(
-    //     target2.split(" - ")[0],
-    //     target2.split(" - ")[1]
-    //   );
-    //   const value1 = interaction.options.getString("value1");
-    //   const value2 = interaction.options.getString("value2");
-
-    //   if (pokemon1.name === pokemon2.name)
-    //     throw new Error("Please pick two different pokemons.");
-
-    //   if (value1 === value2)
-    //     throw new Error("Please pick two different values to compare.");
-
-    //   if (value1 === "psa9" && value2 === "psa10")
-    //     throw new Error(
-    //       "PSA9/PS10 is not supported. Pick PSA10 first then PS9."
-    //     );
-
-    //   let value = ((pokemon1[value1] - 20) / pokemon2[value2]) * 100 - 100;
-
-    //   const formattedPokemon =
-    //     "\n" +
-    //     "Spread of: " +
-    //     pokemon1.name +
-    //     value1 +
-    //     "  $" +
-    //     pokemon1[value1] +
-    //     " and " +
-    //     pokemon2.name +
-    //     value2 +
-    //     " $" +
-    //     pokemon2[value2] +
-    //     " :" +
-    //     "\n" +
-    //     "%" +
-    //     value.toFixed(2);
-
-    //   await interaction.reply(formattedPokemon);
-    // }
-    // if (interaction.commandName === "totalmarketcap") {
-    //   const set = interaction.options.getString("set");
-    //   const reason = interaction.options.getString("value");
-    //   const popValue = reason.substring(3);
-    //   const pop = `pop${popValue}`;
-
-    //   let marketCaps = data
-    //     .find((d) => d.name === set)
-    //     .cards.map((card) => {
-    //       card[reason] * card[pop];
-    //     });
-
-    //   const totalMarketCap = marketCaps.reduce((a, b) => a + b);
-
-    //   const result =
-    //     "\n" + "Total Marketcap for: " + set + "\n" + "$" + totalMarketCap;
-
-    //   await interaction.reply(result);
-    // }
-    // if (interaction.commandName === "marketcap") {
-    //   const pokemon = interaction.options.getString("pokemon");
-    //   const value = interaction.options.getString("value");
-    //   const popValue = reason.substring(3);
-    //   const pop = `pop${popValue}`;
-
-    //   const caughtPokemon = findCard(
-    //     pokemon.split(" - ")[0],
-    //     pokemon.split(" - ")[1]
-    //   );
-
-    //   const marketcap = caughtPokemon[reason] * caughtPokemon[pop];
-
-    //   const formattedPokemon =
-    //     "\n" +
-    //     "Name: " +
-    //     caughtPokemon["name"] +
-    //     "\n" +
-    //     "Marketcap is: " +
-    //     "$" +
-    //     marketcap;
-
-    //   await interaction.reply(formattedPokemon);
-    // }
-    if (interaction.commandName === "price") {
-      const pokemon = interaction.options.getString("base");
-      console.log("pokemon is: ", pokemon);
-      const value = interaction.options.getString("value");
-      console.log("value is: ", value);
-
-      const caughtPokemon = findCard(
-        pokemon.split(" - ")[0],
-        pokemon.split(" - ")[1]
-      );
-
-      console.log("caughtPokemon is: ", caughtPokemon);
-
-      const formattedPokemon =
-        "\n" +
-        "Name: " +
-        caughtPokemon["name"] +
-        "\n" +
-        "Current " +
-        reason +
-        " Price: " +
-        "$" +
-        caughtPokemon[value] +
-        "\n";
-
-      await interaction.reply(formattedPokemon);
+    if (interaction.isChatInputCommand()) {
+      await execute(interaction);
     }
-    // if (interaction.commandName === "population") {
-    //   const pokemon = interaction.options.getString("pokemon");
-    //   const value = interaction.options.getString("value");
+    // if (interaction.commandName === "price") {
+    // const pokemon = interaction.options.getString("base");
+    // console.log("pokemon is: ", pokemon);
+    // const value = interaction.options.getString("value");
+    // console.log("value is: ", value);
 
-    //   const caughtPokemon = findCard(
-    //     pokemon.split(" - ")[0],
-    //     pokemon.split(" - ")[1]
-    //   );
+    // const caughtPokemon = findCard(
+    //   pokemon.split(" - ")[0],
+    //   pokemon.split(" - ")[1]
+    // );
 
-    //   const formattedPokemon =
-    //     "\n" +
-    //     "Name: " +
-    //     caughtPokemon["name"] +
-    //     "\n" +
-    //     reason +
-    //     ": " +
-    //     caughtPokemon[value] +
-    //     "\n";
+    // console.log("caughtPokemon is: ", caughtPokemon);
 
-    //   await interaction.reply(formattedPokemon);
-    // }
-    // if (interaction.commandName === "weight") {
-    //   const setName = interaction.options.getString("set");
-    //   const weightClass = interaction.options.getString("weight");
+    // const formattedPokemon =
+    //   "\n" +
+    //   "Name: " +
+    //   caughtPokemon["name"] +
+    //   "\n" +
+    //   "Current " +
+    //   reason +
+    //   " Price: " +
+    //   "$" +
+    //   caughtPokemon[value] +
+    //   "\n";
 
-    //   const targetSetData = sets.find((s) => s.name === setName);
-
-    //   const result =
-    //     "\n" +
-    //     "Set: " +
-    //     targetSetData?.name +
-    //     "\n" +
-    //     "Average " +
-    //     weightClass +
-    //     " pack: " +
-    //     targetSetData[weightClass] +
-    //     "\n";
-
-    //   await interaction.reply(result);
-    // }
-    // if (interaction.commandName === "compareall") {
-    //   const set = interaction.options.getString("set");
-
-    //   let formattedReply = "";
-
-    //   const differencePokemons = data
-    //     .find((d) => d.name === set)
-    //     ?.cards?.map((card) => {
-    //       const psa10 = card["psa10"] - 20;
-    //       const psa9 = card["psa9"];
-    //       const result = (psa10 / psa9) * 100 - 100;
-
-    //       return [card["name"], result.toFixed(2)];
-    //     })
-    //     .sort(function (a, b) {
-    //       return b[1] - a[1];
-    //     });
-
-    //   differencePokemons.forEach((pokemon) => {
-    //     const tempStr = pokemon[0] + ": " + pokemon[1] + "%" + "\n";
-    //     return (formattedReply += tempStr);
-    //   });
-
-    //   await interaction.reply(formattedReply);
-    // }
-    // if (interaction.commandName === "psacomparison") {
-    //   const pokemon = interaction.options.getString("pokemon");
-    //   const value1 = interaction.options.getString("value1");
-    //   const value2 = interaction.options.getString("value2");
-
-    //   const caughtPokemon = findCard(
-    //     pokemon.split(" - ")[0],
-    //     pokemon.split(" - ")[1]
-    //   );
-
-    //   if (value1 === value2)
-    //     throw new Error("Please pick two different values to compare.");
-
-    //   if (value1 === "psa9" && value2 === "psa10")
-    //     throw new Error(
-    //       "PSA9/PS10 is not supported. Pick PSA10 first then PS9."
-    //     );
-
-    //   let result =
-    //     ((caughtPokemon[value1] - 20) / caughtPokemon[value2]) * 100 - 100;
-
-    //   const formattedPokemon =
-    //     "\n" +
-    //     caughtPokemon["name"] +
-    //     " " +
-    //     value1 +
-    //     " with " +
-    //     value2 +
-    //     ":\n" +
-    //     result.toFixed(2) +
-    //     "%";
-
-    //   await interaction.reply(formattedPokemon);
+    // await interaction.reply(formattedPokemon);
     // }
   } catch (error) {
     console.error(error);
--- src/commands/pokemonprice.js
+++ src/commands/pokemonprice.js
@@ -1,191 +1,126 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-// import data from "../../data/data.json" assert { type: "json" };
+import data from "../../data/data.json" assert { type: "json" };
 
-// // for now supports one set
-// const PokemonPriceCommand = new SlashCommandBuilder()
-//   .setName("price")
-//   .setDescription("Gives you individual pokemon price")
-//   .addStringOption((option) =>
-//     option
-//       .setName("pokemon")
-//       .setDescription("Select the pokemon")
-//       .setAutocomplete(true)
-//   )
-//   .setRequired(true)
-
-//   .addStringOption((option) =>
-//     option
-//       .setName("value")
-//       .setDescription("Select value")
-//       .addChoices(
-//         { name: "psa10", value: "psa10" },
-//         { name: "psa9", value: "psa9" },
-//         { name: "raw", value: "raw" }
-//       )
-//       .setRequired(true)
-//   );
-// export default PokemonPriceCommand.toJSON();
+const findCard = (name, set) => {
+  const setFound = data.find((d) => d.name === set);
+  const cardsInSet = setFound?.cards;
+  const card = cardsInSet.find((card) => card.name === name);
+  return card;
+};
 
 export const PokemonPriceCommand = new SlashCommandBuilder()
   .setName("price")
-  .setDescription("Gives you individual pokemon price")
-  .addStringOption(
-    (option) =>
-      option
-        .setName("base")
-        .setDescription("Search a card in Base Set 1st Edition")
-        .setAutocomplete(true)
-    // .setRequired(true)
+  .setDescription(
+    "Gives you individual pokemon price, pick only one set after you pick value"
   )
-  // .addStringOption(
-  //   (option) =>
-  //     option
-  //       .setName("jungle")
-  //       .setDescription("Search a card in Jungle 1st Edition")
-  //       .setAutocomplete(true)
-  //   // .setRequired(true)
-  // )
-  .addStringOption(
-    (option) =>
-      option
-        .setName("value")
-        .setDescription("Select value")
-        .addChoices(
-          { name: "psa10", value: "psa10" },
-          { name: "psa9", value: "psa9" },
-          { name: "raw", value: "raw" }
-        )
-    // .setRequired(true)
+  .addStringOption((option) =>
+    option
+      .setName("value")
+      .setDescription("Select value")
+      .addChoices(
+        { name: "psa10", value: "psa10" },
+        { name: "psa9", value: "psa9" },
+        { name: "raw", value: "raw" }
+      )
+      .setRequired(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("base")
+      .setDescription("Search a card in Base Set 1st Edition")
+      .setMinLength(1)
+      .setAutocomplete(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("jungle")
+      .setDescription("Search a card in Jungle 1st Edition")
+      .setMinLength(1)
+      .setAutocomplete(true)
   )
   .toJSON();
 
 export const autocomplete = async (interaction) => {
   console.log("inside autocomplete");
-  const focusedOption = interaction.options.getFocused();
-  console.log("this is the focused option: ", focusedOption);
-  const choices = [
-    "Charizard Holo 4",
-    "Blastoise Holo 2",
-    "Venusaur Holo 15",
-    "Nidoking Holo 11",
-    "Chansey Holo 3",
-    "Clefairy Holo 5",
-    "Alakazam Holo 1",
-    "Poliwrath Holo 13",
-    "Raichu Holo 14",
-    "Ninetales Holo 12",
-    "Hitmonchan Holo 7",
-    "Mewtwo Holo 10",
-    "Zapdos Holo 16",
-    "Gyarados Holo 6",
-    "Magneton Holo 9",
-  ];
+  const focusedOption = interaction.options.getFocused(true);
+
+  let choices = null;
+
+  if (focusedOption.name === "base") {
+    choices = [
+      "Charizard Holo 4",
+      "Blastoise Holo 2",
+      "Venusaur Holo 15",
+      "Nidoking Holo 11",
+      "Chansey Holo 3",
+      "Clefairy Holo 5",
+      "Alakazam Holo 1",
+      "Poliwrath Holo 13",
+      "Raichu Holo 14",
+      "Ninetales Holo 12",
+      "Hitmonchan Holo 7",
+      "Mewtwo Holo 10",
+      "Zapdos Holo 16",
+      "Gyarados Holo 6",
+      "Magneton Holo 9",
+    ];
+  }
+
+  if (focusedOption.name === "jungle") {
+    choices = [
+      "Electrode Holo 2",
+      "Flareon Holo 3",
+      "Jolteon Holo 4",
+      "Kangaskhan Holo 5",
+      "Mr. Mime Holo 6",
+      "Nidoqueen Holo 7",
+      "Pidgeot Holo 8",
+      "Pinsir Holo 9",
+      "Scyther Holo 10",
+      "Snorlax Holo 11",
+      "Vaporeon Holo 12",
+      "Venomoth Holo 13",
+      "Victreebel Holo 14",
+      "Vileplume Holo 15",
+      "Wigglytuff Holo 16",
+    ];
+  }
 
   const filtered = choices.filter((choice) =>
-    choice.toLowerCase().startsWith(focusedOption)
+    choice.toLowerCase().startsWith(focusedOption.value)
   );
-  // const filtered = choices.filter((choice) =>
-  //   choice.startsWith(focusedOption.value)
-  // );
   console.log("these are the filtered choices: ", filtered);
   await interaction.respond(
     filtered.map((choice) => ({ name: choice, value: choice }))
   );
 };
 
-// if (focusedOption.name === "jungle") {
-//   choices = [
-//     "Electrode Holo 2",
-//     "Flareon Holo 3",
-//     "Jolteon Holo 4",
-//     "Kangaskhan Holo 5",
-//     "Mr. Mime Holo 6",
-//     "Nidoqueen Holo 7",
-//     "Pidgeot Holo 8",
-//     "Pinsir Holo 9",
-//     "Scyther Holo 10",
-//     "Snorlax Holo 11",
-//     "Vaporeon Holo 12",
-//     "Venomoth Holo 13",
-//     "Victreebel Holo 14",
-//     "Vileplume Holo 15",
-//     "Wigglytuff Holo 16",
-//   ];
-// }
-// console.log("these are the choices: ", choices);
+export const execute = async (interaction) => {
+  const pokemonBase = interaction.options.getString("base");
+  const pokemonJungle = interaction.options.getString("jungle");
 
-// };
-// module.exports = {
-//   data: new SlashCommandBuilder()
-//     .setName("price")
-//     .setDescription("Gives you individual pokemon price")
-//     .addStringOption((option) =>
-//       option
-//         .setName("set")
-//         .setDescription("Select the set")
-//         .setAutocomplete(true)
-//     )
-//     .setRequired(true)
+  if (pokemonBase && pokemonJungle) {
+    throw new Error("You can only search one set at a time");
+  }
 
-//     .addStringOption((option) =>
-//       option
-//         .setName("value")
-//         .setDescription("Select value")
-//         .addChoices(
-//           { name: "psa10", value: "psa10" },
-//           { name: "psa9", value: "psa9" },
-//           { name: "raw", value: "raw" }
-//         )
-//         .setRequired(true)
-//     ),
-//   async autocomplete(interaction) {
-//     const focusedOption = interaction.options.getFocused(true);
-//     let choices;
+  const pokemon = pokemonBase || pokemonJungle;
+  const set = pokemonBase ? "Base Set 1st Edition" : "Jungle 1st Edition";
+  const value = interaction.options.getString("value");
 
-//     if (focusedOption.name === "Base Set 1st Edition") {
-//       choices = [
-//         "Charizard Holo 4",
-//         "Blastoise Holo 2",
-//         "Venusaur Holo 15",
-//         "Nidoking Holo 11",
-//         "Chansey Holo 3",
-//         "Clefairy Holo 5",
-//         "Alakazam Holo 1",
-//         "Poliwrath Holo 13",
-//         "Raichu Holo 14",
-//         "Ninetales Holo 12",
-//         "Hitmonchan Holo 7",
-//         "Mewtwo Holo 10",
-//         "Zapdos Holo 16",
-//         "Gyarados Holo 6",
-//         "Magneton Holo 9",
-//       ];
-//     }
+  console.log("pokemon is: ", pokemon);
+  const caughtPokemon = findCard(pokemon, set);
 
-//     if (focusedOption.name === "Jungle 1st Edition") {
-//       choices = [
-//         "Electrode Holo 2",
-//         "Flareon Holo 3",
-//         "Jolteon Holo 4",
-//         "Kangaskhan Holo 5",
-//         "Mr. Mime Holo 6",
-//         "Nidoqueen Holo 7",
-//         "Pidgeot Holo 8",
-//         "Pinsir Holo 9",
-//         "Scyther Holo 10",
-//         "Snorlax Holo 11",
-//         "Vaporeon Holo 12",
-//         "Venomoth Holo 13",
-//         "Victreebel Holo 14",
-//         "Vileplume Holo 15",
-//         "Wigglytuff Holo 16",
-//       ];
-//     }
-//     const filtered = choices.filter((choice) =>
-//       choice.startsWith(focusedOption.value)
-//     );
-//     await interaction.respond(
-//       filtered.map((choice) => ({ name: choice, value: choice }))
-//     );
-//   },
-// };
+  const formattedPokemon =
+    "\n" +
+    "Name: " +
+    caughtPokemon["name"] +
+    "\n" +
+    "Current " +
+    value +
+    " Price: " +
+    "$" +
+    caughtPokemon[value] +
+    "\n";
+
+  await interaction.reply(formattedPokemon);
+};
