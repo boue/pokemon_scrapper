5d0c13d76bb64be6eb71b4090bb6ab9fe85d3373
cosmicThreePointO <cosmicvoid@mechverse.co>
2023-03-17 16:24:43 -0700

    market summary

--- src/commands/marketsummary.js
+++ src/commands/marketsummary.js
@@ -18,19 +18,34 @@ export const MarketSummaryCommand = new SlashCommandBuilder()
       .setMinLength(1)
       .setAutocomplete(true)
   )
+  .addStringOption((option) =>
+    option
+      .setName("silvertempest")
+      .setDescription("Search a card in Silver Tempest set")
+      .setMinLength(1)
+      .setAutocomplete(true)
+  )
   .toJSON();
 
 export const execute = async (interaction) => {
   const pokemonBase = interaction.options.getString("base");
   const pokemonJungle = interaction.options.getString("jungle");
+  const pokemonSilverTempest = interaction.options.getString("silvertempest");
 
-  if (pokemonBase && pokemonJungle) {
+  if (
+    (pokemonBase || pokemonJungle || pokemonSilverTempest) &&
+    (pokemonBase || pokemonJungle || pokemonSilverTempest)
+  ) {
     await interaction.editReply("You can only search one pokemon at a time");
     throw new Error("You can only search one pokemon at a time");
   }
 
-  const pokemon = pokemonBase || pokemonJungle;
-  const set = pokemonBase ? "Base Set 1st Edition" : "Jungle 1st Edition";
+  const pokemon = pokemonBase || pokemonJungle || pokemonSilverTempest;
+  const set = pokemonBase
+    ? "Base Set 1st Edition"
+    : pokemonJungle
+    ? "Jungle 1st Edition"
+    : "Silver Tempest";
   const { name, psa10, psa9, pop10, pop9, raw } = findCard(pokemon, set);
 
   const formattedPokemons =
